c0eb2ddff6c6bd4edaad26d9ca02e29dc39bc84e
Mehak <mehakkhemani04@gmail.com>
2025-08-26 14:47:06 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,22 +1,30 @@
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {};
+
+// // export default nextConfig;
+
+
+
+
+
+// // next.config.js Exporting Files...
+
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+// const nextConfig = {
+//     output: 'export',
+//     images: {
+//       unoptimized: true, // Disable optimization for static export
+//       basePath: '/youngceoentertainment',
 
-// export default nextConfig;
-
-
-
-
-
-// next.config.js Exporting Files...
-
+//     },
+//   };
+  
+//   module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true, // Disable optimization for static export
-      basePath: '/youngceoentertainment',
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/youngceoentertainment',
+};
 
-    },
-  };
-  
-  module.exports = nextConfig;
+module.exports = nextConfig;
